ef1abe5a085eabe10ca17814b71c4fb2c2ecebc2
JESSIANE ZACARIAS <jessianezacarias@gmail.com>
2024-01-03 22:15:56 -0300

    Criando controller, enums, e entidades

---
+++ Domain/Enums/ETipoVeiculo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Enums
+{
+    public enum ETipoVeiculo
+    {
+        Fusion = 1,
+        Cruze = 2,
+        Roadster = 3, 
+        Tucson = 4,
+        Hatch = 5,
+    }
+}
--- Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7025d80467998de12d6f672d548fa2eb4f40627")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
