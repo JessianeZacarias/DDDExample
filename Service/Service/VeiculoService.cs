7efba815c0328b57e8b419523a3f6e8469c37ac4
JESSIANE ZACARIAS <jessianezacarias@gmail.com>
2024-01-04 22:06:01 -0300

    Criação da Service e Interface

---
+++ Service/Service/VeiculoService.cs
@@ -0,0 +1,32 @@
+﻿using Domain.Commands;
+using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Service
+{
+    public class VeiculoService : IVeiculoService
+    {
+        public void GetAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task PostAsync(VeiculoCommand command)
+        {
+            //Todo
+            //Incluir validação, só podem cadastrar veículos com até 5 anos de uso
+            //Incluir somente carros do tipo SUV, Sedan e Hatch
+
+            throw new NotImplementedException();
+        }
+
+        public void PostAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
